dd99a7a8f210bb1eced1c7047a197cb91aa197c3
Ilan Amzallag <Ilan@air-de-amzallag.lan>
2020-01-25 19:08:17 +0100

    Updates

---
+++ precache-manifest.262bb319450ff7fc727bd8a401d6e461.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90306ac86978e78868dbc4c3d7e9cbe9",
+    "url": "/site-yami/index.html"
+  },
+  {
+    "revision": "149911ed2961f4af4180",
+    "url": "/site-yami/static/css/main.f30cbacf.chunk.css"
+  },
+  {
+    "revision": "eb3335b92d2a5078b21e",
+    "url": "/site-yami/static/js/2.f6d6b4de.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/site-yami/static/js/2.f6d6b4de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "149911ed2961f4af4180",
+    "url": "/site-yami/static/js/main.de1f7d82.chunk.js"
+  },
+  {
+    "revision": "d4a9fbef79e2d99b2b4f",
+    "url": "/site-yami/static/js/runtime-main.f70016d6.js"
+  },
+  {
+    "revision": "bd969448d240700c02755b7fa9cb9c6f",
+    "url": "/site-yami/static/media/APP.bd969448.png"
+  },
+  {
+    "revision": "42fcf10180d675f10b27a34338af2207",
+    "url": "/site-yami/static/media/BD.42fcf101.png"
+  },
+  {
+    "revision": "3fe780c74cdc5834c594a75e760316dc",
+    "url": "/site-yami/static/media/CLIP.3fe780c7.png"
+  },
+  {
+    "revision": "2dd1b41b98bdab09e31ed79844c3e3c2",
+    "url": "/site-yami/static/media/CM.2dd1b41b.png"
+  },
+  {
+    "revision": "6ea90bc187aaeed8fa5073bd06959262",
+    "url": "/site-yami/static/media/PUB.6ea90bc1.png"
+  },
+  {
+    "revision": "5332c3b1f8cf2e9bd1d35d720aa75e0c",
+    "url": "/site-yami/static/media/believe.5332c3b1.png"
+  },
+  {
+    "revision": "d249aca54c4178edca54b4a60a622adf",
+    "url": "/site-yami/static/media/heros.d249aca5.png"
+  },
+  {
+    "revision": "f98d8142fdcbded83696784391f2ecaa",
+    "url": "/site-yami/static/media/lookaway.f98d8142.png"
+  },
+  {
+    "revision": "69fe2f5989e8fc5ea0964948493ac732",
+    "url": "/site-yami/static/media/onimprovise.69fe2f59.png"
+  },
+  {
+    "revision": "270f6ca568d7baae30dd50d470557367",
+    "url": "/site-yami/static/media/palante.270f6ca5.png"
+  },
+  {
+    "revision": "8804748d884c2761d595a2ea682d9d03",
+    "url": "/site-yami/static/media/petitpas.8804748d.png"
+  },
+  {
+    "revision": "e48f18e06561b5ff778bf46c7ea90d90",
+    "url": "/site-yami/static/media/photoprofil.e48f18e0.jpg"
+  }
+]);
